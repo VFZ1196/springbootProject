e2c1738dfdaddab12f8c99147e319fad15f45466
arjun <arjunyadav102030@gmail.com>
2021-01-07 14:26:03 +0530

    initial commit

--- spring-boot-crud-operation/src/main/java/com/javatpoint/repository/BooksRepository.java
+++ spring-boot-crud-operation/src/main/java/com/javatpoint/repository/BooksRepository.java
@@ -11,7 +11,7 @@ import com.javatpoint.model.Books;
 @Repository
 public interface BooksRepository extends JpaRepository<Books, Long> {
 	
-	//@Query(value="select * from books order by book_id Desc", nativeQuery=true)
+	@Query(value="select * from books order by book_id Desc", nativeQuery=true)
 	
 	//@Query(value="select * from books order by price DESC", nativeQuery=true)
 	
@@ -22,7 +22,7 @@ public interface BooksRepository extends JpaRepository<Books, Long> {
 	//@Query(value="select * from books where price>450", nativeQuery = true)
 	
 	//This query is not working why?
-	@Query(value="select books.book_name from books where book_id=48", nativeQuery=true)
+	//@Query(value="select books.book_name from books where book_id=48", nativeQuery=true)
 		
     List<Books> findByOrderByBookId();
 
